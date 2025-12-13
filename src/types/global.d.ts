1c5894ea38d806f8a992d1cd02beeb8ac1c1ca42
apoint123 <108002475+apoint123@users.noreply.github.com>
2025-12-14 02:22:34 +0800

    chore: 优化一些类型

---
+++ src/types/global.d.ts
@@ -0,0 +1,21 @@
+declare module "*.scss" {
+	const content: string;
+	export default content;
+}
+
+declare module "*.css" {
+	const content: string;
+	export default content;
+}
+
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.webp";
+declare module "*.svg";
+
+declare module "*.html" {
+	const content: string;
+	export default content;
+}
